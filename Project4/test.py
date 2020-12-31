ed363a5e12ecc70cb75b90800d352b4874ef09cb
sami495 <76673514+sami495@users.noreply.github.com>
2020-12-31 23:08:44 +0530

    Update test.py

--- Project4/test.py
+++ Project4/test.py
@@ -1,12 +1,12 @@
 import time					  													## Import time library
-import RPi.GPIO as GPIO       													## Import GPIO library
+import RPi.GPIO as GPIO       								## Import GPIO library
 
-GPIO.setmode(GPIO.BOARD)      													## Use board pin numbering
-GPIO.setwarnings(False)       													## Ignore warning for now
+GPIO.setmode(GPIO.BOARD)      								## Use board pin numbering
+GPIO.setwarnings(False)       								## Ignore warning for now
 
-pinArray = [11, 13, 15, 19, 21]   												## declare connected GPIO pin
+pinArray = [11, 13, 15, 19, 21]   							## declare connected GPIO pin
 
-GPIO.setup(pinArray[0], GPIO.OUT) 												## Setup GPIO Pins to OUT
+GPIO.setup(pinArray[0], GPIO.OUT) 							## Setup GPIO Pins to OUT
 GPIO.setup(pinArray[1], GPIO.OUT)
 GPIO.setup(pinArray[2], GPIO.OUT)
 GPIO.setup(pinArray[3], GPIO.OUT)
@@ -22,23 +22,23 @@ def dec2bin():
 
 	   bin_chunks = [bin_dec[i:i+1] for i in range(0, len(bin_dec), 1)]    		## get binary each bit value to string bin_chunk array
 
-	   led5 = int(bin_chunks[0])                                                ## convert string values to int values
+	   led5 = int(bin_chunks[0])                                                    ## convert string values to int values
 	   led4 = int(bin_chunks[1])
 	   led3 = int(bin_chunks[2])
 	   led2 = int(bin_chunks[3])
 	   led1 = int(bin_chunks[4])
 
-	   GPIO.output(pinArray[0],led1)                                            ## Turn on LEDs Acoring to binary values
+	   GPIO.output(pinArray[0],led1)                                                 ## Turn on LEDs Acoring to binary values
 	   GPIO.output(pinArray[1],led2)
 	   GPIO.output(pinArray[2],led3)
 	   GPIO.output(pinArray[3],led4)
 	   GPIO.output(pinArray[4],led5)
 
-	   menu()                                                                   ## return to menu
+	   menu()                                                                        ## return to menu
 
 	else:
-	   print("You have to enter key (0 <= key <= 31)")                          ## get out of range WARNING message
-	   dec2bin()                                                                ## return
+	   print("You have to enter key (0 <= key <= 31)")                          	 ## get out of range WARNING message
+	   dec2bin()                                                                	 ## return
 
 def user2bin():
 	reset()
@@ -56,19 +56,19 @@ def user2bin():
 	   led2 = int(bin_chunks[3])
 	   led1 = int(bin_chunks[4])
 
-	   GPIO.output(pinArray[0],led1)  											## Turn on LEDs
+	   GPIO.output(pinArray[0],led1)  						  ## Turn on LEDs
 	   GPIO.output(pinArray[1],led2)
 	   GPIO.output(pinArray[2],led3)
 	   GPIO.output(pinArray[3],led4)
 	   GPIO.output(pinArray[4],led5)
 
-	   menu()																	## return to menu
+	   menu()									  ## return to menu
 
 	else:
-	   print("You have to enter key (0 <= key <= 31)")                          ## get out of range WARNING message
+	   print("You have to enter key (0 <= key <= 31)")                                ## get out of range WARNING message
 
-	   for i in range(5):                                                       ## Blink five times LEDs
-		   GPIO.output(pinArray[0],True)  										## Turn on LEDs
+	   for i in range(5):                                                             ## Blink five times LEDs
+		   GPIO.output(pinArray[0],True)  					  ## Turn on LEDs
 		   GPIO.output(pinArray[1],True)
 		   GPIO.output(pinArray[2],True)
 		   GPIO.output(pinArray[3],True)
@@ -76,7 +76,7 @@ def user2bin():
 
 		   time.sleep(0.5)
 
-		   GPIO.output(pinArray[0],False)  										## Turn on LEDs
+		   GPIO.output(pinArray[0],False)  					  ## Turn on LEDs
 		   GPIO.output(pinArray[1],False)
 		   GPIO.output(pinArray[2],False)
 		   GPIO.output(pinArray[3],False)
@@ -84,13 +84,13 @@ def user2bin():
 
 		   time.sleep(0.5)
 
-	   dec2bin()
+	   user2bin()
 
-def reset():                                                                    ## rest the LEDs (Turn off all)
+def reset():                                                                              ## rest the LEDs (Turn off all)
 	for x in pinArray:
 		GPIO.output(x,False)
 
-def toRight():                                                                  ## Sweeping light movement from right to left
+def toRight():                                                                            ## Sweeping light movement from right to left
 	print("Press Ctrl-C to terminate the Run")
 	reset()
 	try:
@@ -101,11 +101,11 @@ def toRight():                                                                  
 				GPIO.output(x,False)
 				time.sleep(0.5)
 
-	except KeyboardInterrupt:                                                   ## If user enter Ctrl-C to terminate the Run and return to menu
+	except KeyboardInterrupt:                                                         ## If user enter Ctrl-C to terminate the Run and return to menu
             menu()
             pass
 
-def toLeft():                                                                   ## Sweeping light movement from left to right
+def toLeft():                                                                             ## Sweeping light movement from left to right
 	print("Press Ctrl-C to terminate the Run")
 	reset()
 	try:
@@ -116,32 +116,32 @@ def toLeft():                                                                   
 				GPIO.output(x,False)
 				time.sleep(0.5)
 
-	except KeyboardInterrupt:													## If user enter Ctrl-C to terminate the Run and return to menu
+	except KeyboardInterrupt:							  ## If user enter Ctrl-C to terminate the Run and return to menu
             menu()
             pass
 
-def sweep():                                                                    ## Continuous sweeping light movement
+def sweep():                                                                              ## Continuous sweeping light movement
 	print("Press Ctrl-C to terminate the Run")
 	reset()
 	try:
 		while True:
-			for x in pinArray:													## Right to left
+			for x in pinArray:						  ## Right to left
 				GPIO.output(x,True)
 				time.sleep(0.5)
 				GPIO.output(x,False)
 				time.sleep(0.5)
 
-			for x in reversed(pinArray):										## Left to Right
+			for x in reversed(pinArray):					  ## Left to Right
 				GPIO.output(x,True)
 				time.sleep(0.5)
 				GPIO.output(x,False)
 				time.sleep(0.5)
 
-	except KeyboardInterrupt:													## If user enter Ctrl-C to terminate the Run and return to menu
+	except KeyboardInterrupt:						          ## If user enter Ctrl-C to terminate the Run and return to menu
             menu()
             pass
 
-def menu():																		## menu
+def menu():										  ## menu
     print ("""
     1.Sequential count in Binary
     2.Sweeping light movement from right to left
@@ -152,7 +152,7 @@ def menu():																		## menu
 		Enter your choice (1-6):
     """)
 
-    ans=input("What would you like to do? ")                                 	## get user input
+    ans=input("What would you like to do? ")                                 	          ## get user input
     if ans=="1":
     	dec2bin()
     if ans=="2":
@@ -166,8 +166,8 @@ def menu():																		## menu
     if ans=="6":
     	print("\n Goodbye")
     	reset()
-    	exit()																	## reset and exit the code
+    	exit()										  ## reset and exit the code
     if ans !="":
     	print("\n Not Valid Choice Try again")
 
-menu();																			## start the programe
+menu();										          ## start the programe
